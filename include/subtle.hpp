2862dbbbc72b023078e07038114877f822bd682a
Anjan Roy <hello@itzmeanjan.in>
2023-02-20 15:04:39 +0400

    constant-time inequality of two unsigned values
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- include/subtle.hpp
+++ include/subtle.hpp
@@ -25,4 +25,20 @@ is_unsigned_equal(const operandT x, const operandT y)
   return d;
 }
 
+// Given two unsigned integers x, y of type operandT ( of bitwidth 8, 16, 32 or
+// 64 ), this routine returns true ( if x != y ) or false ( in case x == y )
+// testing inequality of two values.
+//
+// We represent truth value using maximum number that can be represented using
+// returnT i.e. all bits of returnT are set to one. While for false value, we
+// set all bits of returnT to zero.
+template<typename operandT, typename returnT>
+static inline constexpr returnT
+is_unsigned_not_equal(const operandT x, const operandT y)
+  requires(std::is_unsigned_v<operandT> && std::is_unsigned_v<returnT>)
+{
+  const returnT z = is_unsigned_equal<operandT, returnT>(x, y);
+  return ~z; // bit-reverse result of equality check
+}
+
 }

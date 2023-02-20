cfbd12e346a0a3d4e1793379f15d73d736370755
Anjan Roy <hello@itzmeanjan.in>
2023-02-20 17:12:36 +0400

    test functional correctness of constant-time `==` operation
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

---
+++ include/test_subtle.hpp
@@ -0,0 +1,35 @@
+#pragma once
+#include "subtle.hpp"
+#include <cassert>
+#include <random>
+
+// Test cases for ensuring functional correctness of constant-time comparison
+// and selection operations
+namespace subtle_test {
+
+// Test functional correctness of constant-time equality operation over unsigned
+// integer types, checking result against native comparison operator ( i.e. == )
+template<typename operandT,
+         typename returnT,
+         const size_t iterations = (1ul << 16)>
+void
+test_ct_eq()
+  requires(std::is_unsigned_v<operandT> && std::is_unsigned_v<returnT>)
+{
+  constexpr returnT truthv = -static_cast<returnT>(1);
+  constexpr returnT falsev = 0;
+
+  std::random_device rd;
+  std::mt19937_64 gen(rd());
+  std::uniform_int_distribution<operandT> dis;
+
+  for (size_t i = 0; i < iterations; i++) {
+    const operandT x = dis(gen);
+    const operandT y = dis(gen);
+
+    const returnT z = subtle::ct_eq<operandT, returnT>(x, y);
+    assert(z == (x == y ? truthv : falsev));
+  }
+}
+
+}

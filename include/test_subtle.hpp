af0f06af40cb621d5480237b00e5c5e00ebcdc6c
Anjan Roy <hello@itzmeanjan.in>
2023-02-20 17:25:58 +0400

    test functional correctness of `ct_ne` function
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- include/test_subtle.hpp
+++ include/test_subtle.hpp
@@ -32,4 +32,29 @@ test_ct_eq()
   }
 }
 
+// Test functional correctness of constant-time inequality operation over
+// unsigned integer types, checking result against native comparison operator
+template<typename operandT,
+         typename returnT,
+         const size_t iterations = (1ul << 16)>
+void
+test_ct_ne()
+  requires(std::is_unsigned_v<operandT> && std::is_unsigned_v<returnT>)
+{
+  constexpr returnT truthv = -static_cast<returnT>(1);
+  constexpr returnT falsev = 0;
+
+  std::random_device rd;
+  std::mt19937_64 gen(rd());
+  std::uniform_int_distribution<operandT> dis;
+
+  for (size_t i = 0; i < iterations; i++) {
+    const operandT x = dis(gen);
+    const operandT y = dis(gen);
+
+    const returnT z = subtle::ct_ne<operandT, returnT>(x, y);
+    assert(z == (x != y ? truthv : falsev));
+  }
+}
+
 }
--- test/main.cpp
+++ test/main.cpp
@@ -25,5 +25,26 @@ main()
   subtle_test::test_ct_eq<uint64_t, uint64_t>();
   std::cout << "[test] Equality Test\n";
 
+  subtle_test::test_ct_ne<uint8_t, uint8_t>();
+  subtle_test::test_ct_ne<uint16_t, uint8_t>();
+  subtle_test::test_ct_ne<uint32_t, uint8_t>();
+  subtle_test::test_ct_ne<uint64_t, uint8_t>();
+
+  subtle_test::test_ct_ne<uint8_t, uint16_t>();
+  subtle_test::test_ct_ne<uint16_t, uint16_t>();
+  subtle_test::test_ct_ne<uint32_t, uint16_t>();
+  subtle_test::test_ct_ne<uint64_t, uint16_t>();
+
+  subtle_test::test_ct_ne<uint8_t, uint32_t>();
+  subtle_test::test_ct_ne<uint16_t, uint32_t>();
+  subtle_test::test_ct_ne<uint32_t, uint32_t>();
+  subtle_test::test_ct_ne<uint64_t, uint32_t>();
+
+  subtle_test::test_ct_ne<uint8_t, uint64_t>();
+  subtle_test::test_ct_ne<uint16_t, uint64_t>();
+  subtle_test::test_ct_ne<uint32_t, uint64_t>();
+  subtle_test::test_ct_ne<uint64_t, uint64_t>();
+  std::cout << "[test] Inequality Test\n";
+
   return 0;
 }

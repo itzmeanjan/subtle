45f7c6a462122a98563ce1533b946af705860c1c
Anjan Roy <hello@itzmeanjan.in>
2023-02-20 17:29:38 +0400

    test functional correctness of `ct_le` function
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- test/main.cpp
+++ test/main.cpp
@@ -46,5 +46,26 @@ main()
   subtle_test::test_ct_ne<uint64_t, uint64_t>();
   std::cout << "[test] Inequality Test\n";
 
+  subtle_test::test_ct_le<uint8_t, uint8_t>();
+  subtle_test::test_ct_le<uint16_t, uint8_t>();
+  subtle_test::test_ct_le<uint32_t, uint8_t>();
+  subtle_test::test_ct_le<uint64_t, uint8_t>();
+
+  subtle_test::test_ct_le<uint8_t, uint16_t>();
+  subtle_test::test_ct_le<uint16_t, uint16_t>();
+  subtle_test::test_ct_le<uint32_t, uint16_t>();
+  subtle_test::test_ct_le<uint64_t, uint16_t>();
+
+  subtle_test::test_ct_le<uint8_t, uint32_t>();
+  subtle_test::test_ct_le<uint16_t, uint32_t>();
+  subtle_test::test_ct_le<uint32_t, uint32_t>();
+  subtle_test::test_ct_le<uint64_t, uint32_t>();
+
+  subtle_test::test_ct_le<uint8_t, uint64_t>();
+  subtle_test::test_ct_le<uint16_t, uint64_t>();
+  subtle_test::test_ct_le<uint32_t, uint64_t>();
+  subtle_test::test_ct_le<uint64_t, uint64_t>();
+  std::cout << "[test] Lesser than equality Test\n";
+
   return 0;
 }

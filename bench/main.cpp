bde9ff9ac0b595cb0ebda60fc4efd12b6fd3b760
Anjan Roy <hello@itzmeanjan.in>
2023-02-20 19:18:03 +0400

    benchmark `ct_ne` against baseline native `!=` operator
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- bench/main.cpp
+++ bench/main.cpp
@@ -9,4 +9,13 @@ BENCHMARK(bench_subtle::native_eq<uint32_t>);
 BENCHMARK(bench_subtle::ct_eq<uint64_t>);
 BENCHMARK(bench_subtle::native_eq<uint64_t>);
 
+BENCHMARK(bench_subtle::ct_ne<uint8_t>);
+BENCHMARK(bench_subtle::native_ne<uint8_t>);
+BENCHMARK(bench_subtle::ct_ne<uint16_t>);
+BENCHMARK(bench_subtle::native_ne<uint16_t>);
+BENCHMARK(bench_subtle::ct_ne<uint32_t>);
+BENCHMARK(bench_subtle::native_ne<uint32_t>);
+BENCHMARK(bench_subtle::ct_ne<uint64_t>);
+BENCHMARK(bench_subtle::native_ne<uint64_t>);
+
 BENCHMARK_MAIN();
--- include/bench_subtle.hpp
+++ include/bench_subtle.hpp
@@ -66,4 +66,64 @@ native_eq(benchmark::State& state)
   }
 }
 
+// Benchmark constant-time inequality check operation
+template<typename operandT>
+void
+ct_ne(benchmark::State& state)
+{
+  std::random_device rd;
+  std::mt19937_64 gen(rd());
+  std::uniform_int_distribution<operandT> dis;
+
+  operandT x = dis(gen);
+  operandT y = dis(gen);
+
+  for (auto _ : state) {
+    const uint32_t z = subtle::ct_ne<operandT, uint32_t>(x, y);
+
+    benchmark::DoNotOptimize(z);
+    benchmark::DoNotOptimize(x);
+    benchmark::DoNotOptimize(y);
+    benchmark::ClobberMemory();
+
+    x += (z >> 31);
+    y -= (z >> 31);
+
+    benchmark::DoNotOptimize(z);
+    benchmark::DoNotOptimize(x);
+    benchmark::DoNotOptimize(y);
+    benchmark::ClobberMemory();
+  }
+}
+
+// Benchmark native inequality check operator
+template<typename operandT>
+void
+native_ne(benchmark::State& state)
+{
+  std::random_device rd;
+  std::mt19937_64 gen(rd());
+  std::uniform_int_distribution<operandT> dis;
+
+  operandT x = dis(gen);
+  operandT y = dis(gen);
+
+  for (auto _ : state) {
+    const uint32_t z = -static_cast<uint32_t>(x != y);
+
+    benchmark::DoNotOptimize(z);
+    benchmark::DoNotOptimize(x);
+    benchmark::DoNotOptimize(y);
+    benchmark::ClobberMemory();
+
+    x += (z >> 31);
+    y -= (z >> 31);
+
+    benchmark::DoNotOptimize(z);
+    benchmark::DoNotOptimize(x);
+    benchmark::DoNotOptimize(y);
+    benchmark::ClobberMemory();
+  }
+}
+
 }

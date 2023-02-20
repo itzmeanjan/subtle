fec10d79b200a5a496a60c4075bfddeac3390420
Anjan Roy <hello@itzmeanjan.in>
2023-02-20 17:32:27 +0400

    test functional correctness of `ct_gt` function
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- test/main.cpp
+++ test/main.cpp
@@ -23,7 +23,7 @@ main()
   subtle_test::test_ct_eq<uint16_t, uint64_t>();
   subtle_test::test_ct_eq<uint32_t, uint64_t>();
   subtle_test::test_ct_eq<uint64_t, uint64_t>();
-  std::cout << "[test] Equality Test\n";
+  std::cout << "[test] Equality test\n";
 
   subtle_test::test_ct_ne<uint8_t, uint8_t>();
   subtle_test::test_ct_ne<uint16_t, uint8_t>();
@@ -44,7 +44,7 @@ main()
   subtle_test::test_ct_ne<uint16_t, uint64_t>();
   subtle_test::test_ct_ne<uint32_t, uint64_t>();
   subtle_test::test_ct_ne<uint64_t, uint64_t>();
-  std::cout << "[test] Inequality Test\n";
+  std::cout << "[test] Inequality test\n";
 
   subtle_test::test_ct_le<uint8_t, uint8_t>();
   subtle_test::test_ct_le<uint16_t, uint8_t>();
@@ -65,7 +65,28 @@ main()
   subtle_test::test_ct_le<uint16_t, uint64_t>();
   subtle_test::test_ct_le<uint32_t, uint64_t>();
   subtle_test::test_ct_le<uint64_t, uint64_t>();
-  std::cout << "[test] Lesser than equality Test\n";
+  std::cout << "[test] Lesser than equality test\n";
+
+  subtle_test::test_ct_gt<uint8_t, uint8_t>();
+  subtle_test::test_ct_gt<uint16_t, uint8_t>();
+  subtle_test::test_ct_gt<uint32_t, uint8_t>();
+  subtle_test::test_ct_gt<uint64_t, uint8_t>();
+
+  subtle_test::test_ct_gt<uint8_t, uint16_t>();
+  subtle_test::test_ct_gt<uint16_t, uint16_t>();
+  subtle_test::test_ct_gt<uint32_t, uint16_t>();
+  subtle_test::test_ct_gt<uint64_t, uint16_t>();
+
+  subtle_test::test_ct_gt<uint8_t, uint32_t>();
+  subtle_test::test_ct_gt<uint16_t, uint32_t>();
+  subtle_test::test_ct_gt<uint32_t, uint32_t>();
+  subtle_test::test_ct_gt<uint64_t, uint32_t>();
+
+  subtle_test::test_ct_gt<uint8_t, uint64_t>();
+  subtle_test::test_ct_gt<uint16_t, uint64_t>();
+  subtle_test::test_ct_gt<uint32_t, uint64_t>();
+  subtle_test::test_ct_gt<uint64_t, uint64_t>();
+  std::cout << "[test] Greater than test\n";
 
   return 0;
 }
